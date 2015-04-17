f87db9b8ce9b3d5be2e6e6274a7235c2cffdf083
Giovanny Suarez <suarezkrac@gmail.com>
2015-04-17 13:15:12 -0500

    Restaurante v!

---
+++ RestauranteUTP/RestauranteUTP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RestauranteUTP
+//
+//  Created by Developer Cymetria on 17/04/15.
+//  Copyright (c) 2015 Mintic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ RestauranteUTP/RestauranteUTP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RestauranteUTP
+//
+//  Created by Developer Cymetria on 17/04/15.
+//  Copyright (c) 2015 Mintic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
