d188a99c00868a3d6de92c3c88e0e39576f229f0
Giovanny Suarez <suarezkrac@gmail.com>
2015-04-17 09:59:07 -0500

    Primer Ejercicio

---
+++ MIPrimeraApp/MIPrimeraApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MIPrimeraApp
+//
+//  Created by Developer Cymetria on 17/04/15.
+//  Copyright (c) 2015 Mintic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MIPrimeraApp/MIPrimeraApp/ViewController.m
@@ -0,0 +1,72 @@
+//
+//  ViewController.m
+//  MIPrimeraApp
+//
+//  Created by Developer Cymetria on 17/04/15.
+//  Copyright (c) 2015 Mintic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSString * name = @"Giovanny Suarez";
+    NSString * introName = @"Mi nombre es:";
+    
+    NSString * stringFull = [introName stringByAppendingString:name];
+    //introName.stringByAppendingString(name);
+    
+    int age = 30;
+    
+    NSLog(@"%@ y tengo %i años", stringFull, age);
+    
+    NSArray * country = @[@"Colombia", @"Ecuador", @"Peru"];
+
+    NSLog(@"%@",[country objectAtIndex:2]);
+    
+    NSMutableArray * cities = [[NSMutableArray alloc] initWithArray:@[@"Pereira"]];
+    
+    [cities addObject:@"Medellin"];
+    
+    NSLog(@"%@", cities);
+
+//    [cities removeObject:@"Pereira"];
+    [cities removeObjectAtIndex:0];
+    
+   NSLog(@"%@", cities);
+    
+NSDictionary * car = @{@"marca": @"mazda", @"modelo": @"2007", @"placa" : @"JJJ123", @"años" : [NSString stringWithFormat:@"%i", age]};
+    
+    //NSString * year = @"2015";
+    //int yearInt = [year floatValue];
+    
+    NSLog(@"%@", car);
+    
+    NSMutableDictionary * user = [[NSMutableDictionary alloc] initWithDictionary:@{@"user": @"Giovanny", @"pass" : @"123"}];
+    
+    [user setObject:@"Medellin" forKey:@"ciudad"];
+    
+    NSLog(@"%@", user);
+    
+    [user setObject:@"Bogota" forKey:@"ciudad"];
+    
+    NSLog(@"%@", user);
+    
+    [user removeObjectForKey:@"pass"];
+    
+    NSLog(@"%@", user);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
