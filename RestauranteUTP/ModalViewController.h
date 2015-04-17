71c9c783f99866503101795caa8120a22215fb06
Giovanny Suarez <suarezkrac@gmail.com>
2015-04-17 15:21:47 -0500

    Carta

---
+++ RestauranteUTP/ModalViewController.h
@@ -0,0 +1,14 @@
+//
+//  ModalViewController.h
+//  RestauranteUTP
+//
+//  Created by Developer Cymetria on 17/04/15.
+//  Copyright (c) 2015 Mintic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+- (IBAction)backToCart:(id)sender;
+
+@end
---
+++ RestauranteUTP/ModalViewController.m
@@ -0,0 +1,41 @@
+//
+//  ModalViewController.m
+//  RestauranteUTP
+//
+//  Created by Developer Cymetria on 17/04/15.
+//  Copyright (c) 2015 Mintic. All rights reserved.
+//
+
+#import "ModalViewController.h"
+
+@interface ModalViewController ()
+
+@end
+
+@implementation ModalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)backToCart:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
